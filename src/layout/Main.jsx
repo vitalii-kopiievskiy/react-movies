45922f8d2dd949a899ab07a7f3ee66ad4281c3b5
vitalii-kopiievskiy <vitalii.kopiievskiy@gmail.com>
2022-07-09 22:58:50 +0300

    Hide API_KEY

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
   state = {
     movies: [],
@@ -10,7 +12,7 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=7986466c&s=matrix')
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((request) => request.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
@@ -18,7 +20,7 @@ class Main extends React.Component {
   searchMovies = (search, type = 'all') => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=7986466c&s=${search}${
+      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
         type !== 'all' ? `&type=${type}` : ''
       }`
     )
